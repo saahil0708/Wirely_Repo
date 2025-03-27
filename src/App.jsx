f26a35a34a9d4a31783692ec5fbdd6a067a4a1ef
saahil0708 <saahil.ind.dgp@gmail.com>
2025-03-27 17:55:01 +0530

    App updated..

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import Model from './Components/Model'
+<<<<<<< Updated upstream
+=======
+import Testimonials from './Components/Testimonials'
+>>>>>>> Stashed changes
 
 export default() => {
     return (

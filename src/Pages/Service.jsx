4a8d65945936105e3d74a973ba5cc547454cb511
Suryavanshi11444 <modi00447@gmail.com>
2025-03-27 17:39:55 +0530

    save all changes

--- src/Pages/Service.jsx
+++ src/Pages/Service.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Service_Categories from '../Components/Service_Categories'
-import ProgressRoadmap from '../Components/Workflow'
+import Timeline from '../Components/Workflow'
 function Service() {
   return (
     <div>
       <Service_Categories />
-      <ProgressRoadmap />
+      <Timeline />
     </div>
   )
 }

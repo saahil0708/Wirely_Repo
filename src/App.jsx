8e67ef1c754140eb228ccced9473fed8a092fb61
KRITIKA1980 <singhkritika0909@gmail.com>
2025-03-27 11:36:26 +0530

    save all changes of files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-export default function() {
-    return (
-       <h1 className="font-[Poppins]">Hello World</h1>
-    )
+import React from 'react'
+
+
+import ProgressRoadmap from './Components/Workflow'
+// import Team from './Components/Service'
+function App() {
+  return (
+    <div>
+       {/* <Team /> */}
+        <ProgressRoadmap />
+         
+    </div>
+  )
 }
+
+export default App
